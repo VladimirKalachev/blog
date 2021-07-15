94974c5a64700bf2e81fe0f38c780336cdc49a20
VladimirKalachev <arrrrgh@inbox.ru>
2021-07-15 12:33:37 +0300

    create:  - about  - header  - footer rewrite main page, home page with header/footer blocks

--- src/main/java/com/javatrain/blog/controllers/MainController.java
+++ src/main/java/com/javatrain/blog/controllers/MainController.java
@@ -12,4 +12,10 @@ public class MainController {
         model.addAttribute("title", "Main page");
         return "home";
     }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About us");
+        return "about";
+    }
 }
