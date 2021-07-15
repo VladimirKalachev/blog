f2e7389871ed6d118be6743647ec09c8d3200244
VladimirKalachev <arrrrgh@inbox.ru>
2021-07-15 15:32:44 +0300

    BlogController send post to server

--- src/main/java/com/javatrain/blog/controllers/BlogController.java
+++ src/main/java/com/javatrain/blog/controllers/BlogController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -13,14 +15,21 @@ public class BlogController {
     private PostRepository postRepository;
 
     @GetMapping("/blog")
-    public String blogMain(Model model){
+    public String blogMain(Model model) {
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
         return "blog-main";
     }
 
     @GetMapping("/blog/add")
-    public String blogAdd(Model model){
-       return "blog-add";
+    public String blogAdd(Model model) {
+        return "blog-add";
+    }
+
+    @PostMapping("/blog-add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String announce, @RequestParam String full_text, Model model) {
+        Post post = new Post(title, announce, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
