fae35b4ad980a640d6f861ba38027204643ed232
VladimirKalachev <arrrrgh@inbox.ru>
2021-07-16 08:26:07 +0300

    Click on "more detail"

--- src/main/java/com/javatrain/blog/controllers/BlogController.java
+++ src/main/java/com/javatrain/blog/controllers/BlogController.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Controller
 public class BlogController {
+
     @Autowired
     private PostRepository postRepository;
 
@@ -26,10 +31,21 @@ public class BlogController {
         return "blog-add";
     }
 
-    @PostMapping("/blog-add")
+    @PostMapping("/blog/add")
     public String blogPostAdd(@RequestParam String title, @RequestParam String announce, @RequestParam String full_text, Model model) {
         Post post = new Post(title, announce, full_text);
         postRepository.save(post);
         return "redirect:/blog";
     }
+    //'/blog/' + ${element.id}
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-details";
+        }
+
+
 }
